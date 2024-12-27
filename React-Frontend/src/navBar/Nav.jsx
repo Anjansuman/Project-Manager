09f5484604db7dd73ad62406db77bb0bf23e55e9
Anjan <anjansuman80@gmail.com>
2024-12-27 13:21:22 +0530

    Giving CSS to Navigation Bar

--- React-Frontend/src/navBar/Nav.jsx
+++ React-Frontend/src/navBar/Nav.jsx
@@ -1,20 +1,21 @@
-
+import "./Nav.css";
 
 export function Nav() {
 
-    return <div>
-        <div>
+    return <div className = "nav">
+        <div className = "logoContainer">
             LOGO |
         </div>
-        <div>
-            <input type="text" placeholder = "Search"/>
+        <div className = "inputContainer">
+            <input type="text" placeholder = "Search" className = "input"/>
         </div>
-        <div>
+        <div className = "buttons">
             <div>Home</div>
             <div>My Projects</div>
             <div>Messaging</div>
             <div>Notifications</div>
             <div>Me</div>
         </div>
+        <div className = "hamburger"></div>
     </div>
 }
