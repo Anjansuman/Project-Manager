b28bd9686fd4ccc5a4aa2e71cde2577f951b7449
Anjan <anjansuman80@gmail.com>
2024-12-27 12:36:27 +0530

    Made a basic structure of Navigation Bar

---
+++ React-Frontend/src/navBar/Nav.jsx
@@ -0,0 +1,20 @@
+
+
+export function Nav() {
+
+    return <div>
+        <div>
+            LOGO |
+        </div>
+        <div>
+            <input type="text" placeholder = "Search"/>
+        </div>
+        <div>
+            <div>Home</div>
+            <div>My Projects</div>
+            <div>Messaging</div>
+            <div>Notifications</div>
+            <div>Me</div>
+        </div>
+    </div>
+}
