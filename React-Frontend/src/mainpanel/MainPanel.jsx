fc0a90cb96131d2c5e768d30b701b0df2f0f3a7b
Anjan <anjansuman80@gmail.com>
2024-12-30 01:51:34 +0530

    refining MainPanel more with the use of useState.

--- React-Frontend/src/mainpanel/MainPanel.jsx
+++ React-Frontend/src/mainpanel/MainPanel.jsx
@@ -1,13 +1,47 @@
-import "./MainPanel.css";
+import React, { useState } from "react";
 import { Blocks } from "./Blocks";
+import { PlusBlock } from "./PlusBlock";
+import { PlusBlockPanel } from "./PlusBlockPanel";
+import "./MainPanel.css";
+
 
 export function MainPanel() {
-    return <div className="panel">
-        <div className="title">
-            Projects
+
+    const [blocks, setBlocks] = useState([]); // State to store blocks
+    const [isPanelVisible, setIsPanelVisible] = useState(false); // State to make panel visible
+
+    function handleAddBlock() {
+        setBlocks([{}, ...blocks]);
+        setIsPanelVisible(false); // this will hide the panel after adding the block
+    }
+
+    // function to show panel
+    function handleShowPanel() {
+        setIsPanelVisible(true);
+    }
+
+    // function to close panel without adding a block
+    function handleClosePanel() {
+        setIsPanelVisible(false);
+    }
+
+    const renderBlock = blocks.map(() => (<Blocks/>));
+
+    // it is used for conditional styling for blocks 
+    const containerClass = {
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: blocks.length > 3 ? "center" : ""
+    }
+
+    return <div className = "panel">
+        <div className = "title">
+            <div>Projects</div>
         </div>
-        <div>
-            <Blocks/>
+        <div style = {containerClass}>
+            <PlusBlock onAddBlock = {handleShowPanel}/>
+            {isPanelVisible && <PlusBlockPanel onAddBlock = {handleAddBlock} onClose = {handleClosePanel}/>}
+            {renderBlock}
         </div>
     </div>
 }
