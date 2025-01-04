587a51c1566731af0b927772b0d8b1282d96fd7d
Anjan <anjansuman80@gmail.com>
2025-01-04 23:30:08 +0530

    Designed a Button for taking inmage input.

---
+++ React-Frontend/src/navBar/ImageInput.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./ImageInput.css";
+
+export function ImageInput() {
+    const [FileName, setFileName] = useState(null);
+
+    function handleFileChange(event) {
+        const file = event.target.files[0];
+        if(file) {
+            setFileName(file.name);
+        }
+    }
+
+    return <div className="image-input-container">
+        <input type="file" accept= "image/*" id="fileInput" className="hidden-file-input" onChange={handleFileChange}/>
+        <label htmlFor="fileInput" className="custom-file-button">
+            +
+        </label>
+    </div>
+}
+
+// I didn't used the FileName till yet, will use it later and this will show which file is selected.
