34dc3fcd4fd723a80506eee786d53c0741182349
Anjan <anjansuman80@gmail.com>
2025-01-15 23:43:37 +0530

    Making personalised home page.

--- React-Frontend/src/Home_Page/Home_Page.jsx
+++ React-Frontend/src/Home_Page/Home_Page.jsx
@@ -1,7 +1,18 @@
+import { Main_Element } from "./Main_Element/Main_Element";
+import { Right_Element } from "./Right_Element";
 
+import { useRecoilValue } from "recoil";
+import { Theme_State } from "../Atoms/Theme_State";
 
-export const Home_Page = () => {
-    return <div className="">
+export function Home() {
 
+    const theme_state = useRecoilValue(Theme_State);
+    const theme_mode = theme_state.mode;
+    const theme = (theme_mode == 'light') ? theme_state.light : theme_state.dark;
+
+    return <div className={`h-screen`}
+        style={{ backgroundColor: theme.background }}
+    >
+        <Main_Element />
     </div>
 }
