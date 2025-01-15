d9cf9e166e9435501277608e7a9ddd321041a5d0
Anjan <anjansuman80@gmail.com>
2025-01-15 23:57:28 +0530

    covering App.jsx in RecoilRoot.

--- React-Frontend/src/App.jsx
+++ React-Frontend/src/App.jsx
@@ -1,14 +1,24 @@
+import { Home } from "./Home_Page/Home_Page";
 import { Nav } from "./navBar/Nav";
 import { Panels } from "./Panels";
 
-function App() {
+import { RecoilRoot } from "recoil";
 
-  return <div style = {{height: "100vh", width: "100vw", backgroundColor: "black", position: "fixed", top: 0, left: 0}}>
-    <div style = {{marginBottom: 10}}>
-      <Nav/>
-    </div>
-    <Panels/>
-  </div>
+function App() {
+  return <RecoilRoot>
+    <Home />
+  </RecoilRoot>
 }
 
 export default App
+
+
+// function App() {
+
+//   return <div style = {{height: "100vh", width: "100vw", backgroundColor: "black", position: "fixed", top: 0, left: 0}}>
+//     <div style = {{marginBottom: 10}}>
+//       <Nav/>
+//     </div>
+//     <Panels/>
+//   </div>
+// }
