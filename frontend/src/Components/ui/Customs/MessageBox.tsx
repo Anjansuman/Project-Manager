2520aa15990299b65afaed53dda383bc37d2940d
Anjan <anjansuman80@gmail.com>
2025-02-19 03:22:58 +0530

    made the Message box for showing time also.

--- frontend/src/Components/ui/Customs/MessageBox.tsx
+++ frontend/src/Components/ui/Customs/MessageBox.tsx
@@ -1,8 +1,8 @@
 
 
-export const MessageBox = ({ text }: { text: string }) => {
-    return <div className="bg-white p-2 pr-5 pb-3 max-w-[50%] inline-block rounded-r-md rounded-bl-md relative">
-        <p className="leading-tight">{text}</p>
-        <span className="absolute bottom-1 right-2 text-[8px] text-gray-500">01:23</span>
+export const MessageBox = ({ text, time }: { text: string, time: string }) => {
+    return <div className="bg-white p-2 pr-5 pb-3 max-w-[70%] inline-block rounded-r-md rounded-bl-md relative">
+        <p className="break-words">{text}</p>
+        <span className="absolute bottom-1 right-2 text-[8px] text-gray-500">{time}</span>
     </div>
 }
--- frontend/src/Components/ui/Customs/ProjectTile.tsx
+++ frontend/src/Components/ui/Customs/ProjectTile.tsx
@@ -1,6 +1,8 @@
 import { useRecoilValue } from "recoil";
 import { ThemeState } from "../../../Atoms/ThemeState";
 
+import { Link } from "react-router-dom";
+
 interface ProjectTileProps {
     title: string,
     image: string,
@@ -16,30 +18,32 @@ export const ProjectTile = ({ title, image, completion }: ProjectTileProps) => {
 
 
     return <div>
-        <div className="h-[213px] w-48 bg-[#653AD847] rounded-3xl mr-2 pt-6 cursor-pointer">
-            <div className="flex justify-center mb-4">
-                <div className="h-24 w-24 bg-[#653AD847] rounded-full shadow-lg flex justify-center items-center">
-                    <div className="h-[90px] w-[90px] bg-[#653AD847] rounded-full shadow-lg flex justify-center items-center overflow-hidden">
-                        {/* <img src={image} alt="" /> */}
-                        {image}
+        <Link to={'/projects/'}>
+            <div className="h-[213px] w-48 bg-[#653AD847] rounded-3xl mr-2 pt-6 cursor-pointer">
+                <div className="flex justify-center mb-4">
+                    <div className="h-24 w-24 bg-[#653AD847] rounded-full shadow-lg flex justify-center items-center">
+                        <div className="h-[90px] w-[90px] bg-[#653AD847] rounded-full shadow-lg flex justify-center items-center overflow-hidden">
+                            {/* <img src={image} alt="" /> */}
+                            {image}
+                        </div>
+                    </div>
+                </div>
+                <div className="flex justify-center text-center text-[17px] font-bold mb-4"
+                    style={{ color: theme.font_color }}
+                >
+                    {title}
+                </div>
+                <div className="flex justify-center">
+                    <div className="h-3 w-32 rounded-full overflow-hidden"
+                        style={{ backgroundColor: theme.dark_panel }}
+                    >
+                        {/* here, make width length as variable and take input from the project details to show the completion bar */}
+                        <div className="h-[100%] bg-[#653AD8] rounded-full "
+                            style={{ width: `${completion}%` || '0' }}
+                        ></div>
                     </div>
                 </div>
             </div>
-            <div className="flex justify-center text-center text-[17px] font-bold mb-4"
-                style={{ color: theme.font_color }}
-            >
-                {title}
-            </div>
-            <div className="flex justify-center">
-                <div className="h-3 w-32 rounded-full overflow-hidden"
-                    style={{ backgroundColor: theme.dark_panel }}
-                >
-                    {/* here, make width length as variable and take input from the project details to show the completion bar */}
-                    <div className="h-[100%] bg-[#653AD8] rounded-full "
-                        style={{ width: `${completion}%` || '0' }}
-                    ></div>
-                </div>
-            </div>
-        </div>
+        </Link>
     </div>
 }
